c24dbed0ec047676d8d5e7be886e7cb580afdb3f
liwen121 <9546005+liwen121@user.noreply.gitee.com>
2022-06-08 19:28:33 +0800

    后台管理系统

---
+++ dev/boss/test.js
@@ -0,0 +1,523 @@
+// // const menu = [
+// //     {
+// //         "id": 1,
+// //         "parentId": -1,
+// //         "href": "",
+// //         "icon": "lock",
+// //         "name": "权限管理",
+// //         "description": "管理系统角色、菜单、资源",
+// //         "orderNum": 1,
+// //         "shown": 1,
+// //         "level": 0,
+// //         "createdTime": "2020-07-20 07:41:38",
+// //         "updatedTime": "2020-07-20 07:41:38",
+// //         "createdBy": "system",
+// //         "updatedBy": "system",
+// //         "subMenuList": [
+// //             {
+// //                 "id": 2,
+// //                 "parentId": 1,
+// //                 "href": "Role",
+// //                 "icon": "lock",
+// //                 "name": "角色列表",
+// //                 "description": "管理系统角色",
+// //                 "orderNum": 1,
+// //                 "shown": 1,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 3,
+// //                 "parentId": 1,
+// //                 "href": "Menu",
+// //                 "icon": "lock",
+// //                 "name": "菜单列表",
+// //                 "description": "管理系统菜单",
+// //                 "orderNum": 2,
+// //                 "shown": 1,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 4,
+// //                 "parentId": 1,
+// //                 "href": "Resource",
+// //                 "icon": "lock",
+// //                 "name": "资源列表",
+// //                 "description": "管理系统资源",
+// //                 "orderNum": 3,
+// //                 "shown": 1,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 10,
+// //                 "parentId": 1,
+// //                 "href": "AllocMenu",
+// //                 "icon": "setting",
+// //                 "name": "给角色分配菜单页面",
+// //                 "description": "给角色分配菜单页面路由",
+// //                 "orderNum": 4,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 11,
+// //                 "parentId": 1,
+// //                 "href": "AllocResource",
+// //                 "icon": "setting",
+// //                 "name": "给角色分配资源页面",
+// //                 "description": "给角色分配资源页面路由",
+// //                 "orderNum": 5,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 12,
+// //                 "parentId": 1,
+// //                 "href": "AddMenu",
+// //                 "icon": "setting",
+// //                 "name": "添加菜单页面",
+// //                 "description": "添加菜单页路由",
+// //                 "orderNum": 6,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 13,
+// //                 "parentId": 1,
+// //                 "href": "UpdateMenu",
+// //                 "icon": "setting",
+// //                 "name": "更新菜单页面",
+// //                 "description": "更新菜单页路由",
+// //                 "orderNum": 7,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 14,
+// //                 "parentId": 1,
+// //                 "href": "ResourceCategory",
+// //                 "icon": "setting",
+// //                 "name": "资源分类列表页面",
+// //                 "description": "资源分类列表页面路由",
+// //                 "orderNum": 8,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 22,
+// //                 "parentId": 1,
+// //                 "href": "addOrder",
+// //                 "icon": "lock",
+// //                 "name": "订单管理修改",
+// //                 "description": "订单系统管理修改",
+// //                 "orderNum": 1,
+// //                 "shown": 0,
+// //                 "level": 0,
+// //                 "createdTime": "2020-08-10 12:32:41",
+// //                 "updatedTime": "2020-08-10 12:32:41",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             }
+// //         ]
+// //     },
+// //     {
+// //         "id": 5,
+// //         "parentId": -1,
+// //         "href": "Courses",
+// //         "icon": "film",
+// //         "name": "课程管理",
+// //         "description": "课程的新增、修改、查看、发布、上下架",
+// //         "orderNum": 2,
+// //         "shown": 1,
+// //         "level": 0,
+// //         "createdTime": "2020-07-20 07:41:38",
+// //         "updatedTime": "2020-07-20 07:41:38",
+// //         "createdBy": "system",
+// //         "updatedBy": "system",
+// //         "subMenuList": [
+// //             {
+// //                 "id": 19,
+// //                 "parentId": 5,
+// //                 "href": "CourseItem",
+// //                 "icon": "setting",
+// //                 "name": "课程详情页面",
+// //                 "description": "课程详情页面路由",
+// //                 "orderNum": 1,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 20,
+// //                 "parentId": 5,
+// //                 "href": "CourseSections",
+// //                 "icon": "setting",
+// //                 "name": "课时信息页面",
+// //                 "description": "课时信息页面路由",
+// //                 "orderNum": 2,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 21,
+// //                 "parentId": 5,
+// //                 "href": "VideoOptions",
+// //                 "icon": "setting",
+// //                 "name": "课时上传视频",
+// //                 "description": "课时上传视频页面路由",
+// //                 "orderNum": 3,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 23,
+// //                 "parentId": 5,
+// //                 "href": "updateStatus修改",
+// //                 "icon": "lock",
+// //                 "name": "课程管理状态修改",
+// //                 "description": "设置课程状态修改",
+// //                 "orderNum": 3,
+// //                 "shown": 1,
+// //                 "level": 0,
+// //                 "createdTime": "2020-08-10 20:32:41",
+// //                 "updatedTime": "2020-08-10 20:32:41",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 24,
+// //                 "parentId": 5,
+// //                 "href": "sectionAndLesson",
+// //                 "icon": "setting",
+// //                 "name": "章节内容与课时内容管理",
+// //                 "description": "管理章节与课时内容内容",
+// //                 "orderNum": 2,
+// //                 "shown": 1,
+// //                 "level": 1,
+// //                 "createdTime": "2020-08-17 10:11:25",
+// //                 "updatedTime": "2020-08-17 10:15:00",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             }
+// //         ]
+// //     },
+// //     {
+// //         "id": 6,
+// //         "parentId": -1,
+// //         "href": "Users",
+// //         "icon": "user",
+// //         "name": "用户管理",
+// //         "description": "用户的查询、禁用、启用",
+// //         "orderNum": 3,
+// //         "shown": 1,
+// //         "level": 0,
+// //         "createdTime": "2020-07-20 07:41:38",
+// //         "updatedTime": "2020-07-20 07:41:38",
+// //         "createdBy": "system",
+// //         "updatedBy": "system",
+// //         "subMenuList": []
+// //     },
+// //     {
+// //         "id": 7,
+// //         "parentId": -1,
+// //         "href": "",
+// //         "icon": "setting",
+// //         "name": "广告管理",
+// //         "description": "广告、广告位管理",
+// //         "orderNum": 4,
+// //         "shown": 1,
+// //         "level": 0,
+// //         "createdTime": "2020-07-20 07:41:38",
+// //         "updatedTime": "2020-07-20 07:41:38",
+// //         "createdBy": "system",
+// //         "updatedBy": "system",
+// //         "subMenuList": [
+// //             {
+// //                 "id": 8,
+// //                 "parentId": 7,
+// //                 "href": "Advertise",
+// //                 "icon": "setting",
+// //                 "name": "广告列表",
+// //                 "description": "广告管理",
+// //                 "orderNum": 1,
+// //                 "shown": 1,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 9,
+// //                 "parentId": 7,
+// //                 "href": "AdvertiseSpace",
+// //                 "icon": "setting",
+// //                 "name": "广告位列表",
+// //                 "description": "广告位管理",
+// //                 "orderNum": 2,
+// //                 "shown": 1,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 15,
+// //                 "parentId": 7,
+// //                 "href": "AddAdvertise",
+// //                 "icon": "setting",
+// //                 "name": "添加广告页面",
+// //                 "description": "添加广告页面路由",
+// //                 "orderNum": 3,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 16,
+// //                 "parentId": 7,
+// //                 "href": "UpdateAdvertise",
+// //                 "icon": "setting",
+// //                 "name": "编辑广告页面",
+// //                 "description": "编辑广告页面路由",
+// //                 "orderNum": 4,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 17,
+// //                 "parentId": 7,
+// //                 "href": "AddAdvertiseSpace",
+// //                 "icon": "setting",
+// //                 "name": "添加广告位页面",
+// //                 "description": "添加广告位页面路由",
+// //                 "orderNum": 5,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             },
+// //             {
+// //                 "id": 18,
+// //                 "parentId": 7,
+// //                 "href": "UpdateAdvertiseSpace",
+// //                 "icon": "setting",
+// //                 "name": "更新广告位页面",
+// //                 "description": "更新广告位页面路由",
+// //                 "orderNum": 6,
+// //                 "shown": 0,
+// //                 "level": 1,
+// //                 "createdTime": "2020-07-20 07:41:38",
+// //                 "updatedTime": "2020-07-20 07:41:38",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             }
+// //         ]
+// //     },
+// //     {
+// //         "id": 25,
+// //         "parentId": -1,
+// //         "href": "user",
+// //         "icon": "icon",
+// //         "name": "人员管理",
+// //         "description": "管理员工",
+// //         "orderNum": 1,
+// //         "shown": 1,
+// //         "level": 0,
+// //         "createdTime": "2020-08-17 10:12:22",
+// //         "updatedTime": "2020-08-17 10:12:22",
+// //         "createdBy": "system",
+// //         "updatedBy": "system",
+// //         "subMenuList": [
+// //             {
+// //                 "id": 26,
+// //                 "parentId": 25,
+// //                 "href": "guanli",
+// //                 "icon": "set",
+// //                 "name": "管理客服人员",
+// //                 "description": "管理客服人员",
+// //                 "orderNum": 0,
+// //                 "shown": 1,
+// //                 "level": 1,
+// //                 "createdTime": "2020-08-17 10:17:13",
+// //                 "updatedTime": "2020-08-17 10:17:13",
+// //                 "createdBy": "system",
+// //                 "updatedBy": "system",
+// //                 "subMenuList": []
+// //             }
+// //         ]
+// //     }
+// // ];
+
+
+
+
+// // for (let key in menu){
+// //     let subMenuList = menu[key].subMenuList;
+
+// //     console.log(key);
+  
+// //     if (key==0) {
+// //         subMenuList.splice(3, 6);
+// //         console.log(subMenuList);
+// //     };
+
+// //     if (key==1) {
+// //         subMenuList.splice(2, 4);
+// //         console.log(subMenuList);
+// //     }
+    
+// //     if (key == 3) {
+// //         console.log("3-------------");
+// //         subMenuList.splice(2,4);
+// //         console.log(subMenuList);
+// //     }
+// // };
+
+// // console.log(menu);
+
+
+
+
+//       //  首字母大写
+      
+// let  username = "user";
+
+
+
+
+// function initAlphabetToUpperCase (Str="") {
+//     let newPageName1 = username.split("", 1);
+//     let newPageName2 = username.slice(1,);
+
+//     console.log(newPageName1);
+//     console.log(newPageName2);
+
+
+//    let StrResult = (username[0]).toUpperCase() + newPageName2;
+
+//     return StrResult;
+// };
+
+// console.log(initAlphabetToUpperCase(username));
+
+
+// // 进行筛选
+// let list = {
+//     1: [2, 3, 4],
+//     5: [0],
+//     6: [0],
+//     7: [8, 9],
+//     25: [25],
+// };
+
+// let menuList = [
+//     {
+//         id: "1",
+//         subMenuItem: [
+//         { id: "2",
+//          subMenuItem:[],},
+//         { id: "3",
+//          subMenuItem:[],},
+//         { id: "4",
+//          subMenuItem:[],},
+//         { id: "5",
+//          subMenuItem:[],},
+//     ]
+//    },
+//     {
+//         id: "5",
+//         subMenuItem: [
+//         { id: "2",
+//          subMenuItem:[],},
+//         { id: "3",
+//          subMenuItem:[],},
+//         { id: "4",
+//          subMenuItem:[],},
+//         { id: "5",
+//          subMenuItem:[],},
+//     ]
+//    },
+// ]
+
+// Object.keys(list).forEach((item) => {
+//     console.log(item);
+//     if (item==) {
+        
+//     }
+// })
+
+
+console.log(1=="1");
